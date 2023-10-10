bf8332ee0803ded364623d3b9aa61a43b121c5ee
DustinHeaps <60202144+DustinHeaps@users.noreply.github.com>
2023-10-10 11:10:53 -0400

    commented out userid

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,11 +1,12 @@
 import { getUserById } from "@/actions/user.action";
 import Question from "@/components/forms/Question";
-import { auth } from "@clerk/nextjs";
+// import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
 const Page = async () => {
-  const { userId } = auth();
+  // const { userId } = auth();
 
+  const userId = '123456789'
   if (!userId) redirect("/sign-in");
 
   const mongoUser = await getUserById({ userId });
